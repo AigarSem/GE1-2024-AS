d25aac9f73e6eff7194820a87e58f69c5a6eee16
AigarSem <C21322311@mytudublin.ie>
2024-09-27 17:11:42 +0100

    Adding Bricks and Color attempt

--- games-engines-2024/scenes/node_3d.gd
+++ games-engines-2024/scenes/node_3d.gd
@@ -4,12 +4,18 @@ extends Node3D
 @export var rows = 10
 @export var cols = 10
 
+# Colors for the bricks
+var blue_material = preload("res://blue.tres")
+var green_material = preload("res://green.tres")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
 	for row in range(rows):
 		for col in range(cols):
 			var brick = brick_scene.instantiate()
+			var mesh_instance = brick.get_node("MeshInstance3D")
+			mesh_instance.material_override = blue_material
 			var pos = Vector3(col, row, 0)
 			brick.position = pos
 			add_child(brick)
