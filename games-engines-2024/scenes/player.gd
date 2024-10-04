896b62ebc7cc7f964779e80e6c92e21283931234
AigarSem <C21322311@mytudublin.ie>
2024-10-04 15:14:04 +0100

    Random Bricks and camera rotation

--- games-engines-2024/scenes/player.gd
+++ games-engines-2024/scenes/player.gd
@@ -1,4 +1,4 @@
-extends MeshInstance3D
+extends CharacterBody3D
 
 
 @export var speed:float = -1
@@ -10,14 +10,27 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+# Physics process can be called more times a second
+func _physics_process(delta: float) -> void:
 	
 	# position.z += speed * delta
 	# global_position.z += speed * delta
 	
 	var f = Input.get_axis("move_back", "move_forward")
 	
-	translate(Vector3(0, 0, f * delta * speed))
+	var v = Vector3()
+	v = global_transform.basis.z
+	
+	print(global_transform.basis.z)
+	
+	velocity = v * speed * f
+	move_and_slide()
+	
+	var r = Input.get_axis("turn_left", "turn_right")
+	
+	rotate_y(deg_to_rad(rot_speed * r * delta))
+	
+	#translate(Vector3(0, 0, f * delta * speed))
 	# rotate_y()
 	# rotate_y(deg_to_rad(rot_speed) * delta)
 	# rotate_x(deg_to_rad(rot_speed) * delta)
