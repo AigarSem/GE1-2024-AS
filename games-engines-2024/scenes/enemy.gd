66e9e12d244ed1fcc68e2928089020cdc0394386
AigarSem <C21322311@mytudublin.ie>
2024-10-25 16:10:19 +0100

    Adjusted code

--- games-engines-2024/scenes/enemy.gd
+++ games-engines-2024/scenes/enemy.gd
@@ -16,25 +16,20 @@ var t = 0
 # Called when the node enters the scene tree for the fdirst time.
 func _ready() -> void:
 	
-	
-	
 	to_player = tank.global_position - global_position
 	to_player = to_player.normalized()
 	forw = global_transform.basis.z
-	
 	axis = to_player.cross(forw)
-	
+	axis = axis.normalized()
 	theta = acos(to_player.dot(forw))
 	
 	# Destination quaternion
-	q2 = Quaternion(axis, theta)
+	q2 = Quaternion(-axis, theta)
 	
 	# Starting quaternion
 	q1 = global_basis.get_rotation_quaternion()
 	
 	t = 0
-	
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,18 +38,9 @@ func _process(delta: float) -> void:
 	var q:Quaternion
 	
 	if (t < 1.0):
-		t = t + delta
+		t = t + delta * 0.1
 	else:
 		t = 1.0
-		
+	
 	q = q1.slerp(q2, t)
 	global_basis = Basis(q)
-	
-	
-	
-	
-	
-	
-	
-	
-	pass
