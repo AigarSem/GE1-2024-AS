de4a7dd8ac6aff819adf96792fa0a37db0c871cb
AigarSem <C21322311@mytudublin.ie>
2024-11-22 16:09:40 +0000

    Sound effects in class

--- games-engines-2024/music_friday.gd
+++ games-engines-2024/music_friday.gd
@@ -14,3 +14,19 @@ func _process(delta: float) -> void:
 func _on_button_button_down() -> void:
 	$AudioStreamPlayer2D.play()
 	pass # Replace with function body.
+
+
+
+# Connects the function of HSlider to this function and use values to change things of the audio
+# Can use the audio busses to change effect e.g. pitch, reverb and delay
+func _on_h_slider_value_changed(value: float) -> void:
+	print(value)
+	$AudioStreamPlayer2D.pitch_scale = value
+	
+	pass # Replace with function body.
+
+
+# When audio finished, play again
+func _on_audio_stream_player_2d_finished() -> void:
+	$AudioStreamPlayer2D.play()
+	pass # Replace with function body.
